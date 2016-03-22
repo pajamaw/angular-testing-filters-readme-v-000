8573159a3e47420f0bf67425dc606443811d2b74
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-03-22 08:47:12 -0400

    Remove code from controller and test so lab is a code-along

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,17 +1,6 @@
 function ContactController($filter) {
-    this.list = [{
-        name: 'Bob'
-    }, {
-        name: 'Tom'
-    }];
 
-    this.search = 'B';
 
-    this.filteredList = $filter('filter')(this.list, this.search);
-
-    this.changeFilter = function () {
-        this.filteredList = $filter('filter')(this.list, this.search);
-    };
 }
 
 angular
